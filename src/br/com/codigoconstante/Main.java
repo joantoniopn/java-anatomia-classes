27e2d492c2aa1b10ed4c443edfc2e5b984e411a8
Joao Neto <joao-antoniopn@live.com>
2023-02-27 20:51:11 -0300

    Imprime mensagem

--- src/br/com/codigoconstante/Main.java
+++ src/br/com/codigoconstante/Main.java
@@ -1,8 +1,7 @@
 package br.com.codigoconstante;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("Olá Mundo, sejam bem-vindos!");
     }
 }
